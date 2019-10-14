3a259305671a59f677f97c555ab80a9f7c318a84
wuh <wuh@changmeng.com>
2019-10-14 20:51:15 +0800

    no message

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -55,18 +55,27 @@ class SiteController extends Controller
 
         for($i = 0 ;$i < 7; $i++){
             //子进程
+
+            /**
+             * 创建子进程
+             *
+             * @param callback 子进程创建成功后要执行的函数
+             * @param bool 重定向子进程的标准输入和输出。启用此选项后，在子进程内输出内容将不是打印屏幕，而是写入到主进程管道。读取键盘输入将变为从管道中读取数据。默认为阻塞读取
+             */
             $process = new \Swoole\Process(function(\Swoole\Process $worker) use($i,$urls){
                 //curl
                 $content = $this->curlData($urls[$i]);
 //        echo $content.PHP_EOL; 两种方法都行
-                $worker->write($content.PHP_EOL);
+                $worker->write($content.PHP_EOL); //写入管道中
             },true);
-            $pid = $process->start();
+            $pid = $process->start();  //创建成功返回子进程的PID，创建失败返回false。可使用swoole_errno和swoole_strerror得到错误码和错误信息。
             $workers[$pid] = $process;
         }
 
         foreach ($workers as $process){
-            echo $process->read();
+            //ps 记得清空子进程
+
+            echo $process->read(); // 从管道中读取
         }
 
         echo "process-end-time:".date("Ymd H:i:s");
