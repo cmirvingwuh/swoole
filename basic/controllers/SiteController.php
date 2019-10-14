31778a6b2507265a66c8f36c39e9d4b3ec7ecfac
wuh <wuh@changmeng.com>
2019-10-14 19:21:56 +0800

    no message

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -37,6 +37,15 @@ class SiteController extends Controller
         ],
     ];
     const PROXY_SERVER      = '180.76.244.131:44446';             //百度智能云代理服务器
+
+
+    public function actionTest()
+    {
+        echo posix_getpid(); // 获取当前进程的 pid
+        swoole_set_process_name('swoole process master'); // 修改所在进程的进程名
+        sleep(100); // 模拟一个持续运行 100s 的程序, 这样就可以在进程中查看到它, 而不是运行完了就结束
+    }
+
     public function actionIndex()
     {
 
